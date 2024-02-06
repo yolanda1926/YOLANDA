d61dbd48e9cd3b6a93ef1e9302feeb33a920afd1
yolanda1926 <159155387+yolanda1926@users.noreply.github.com>
2024-02-06 12:26:14 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function teste(){
+
+    let nome = document.getElementById("nome").value;
+ let senha =  document.getElementById("nome").value;
+
+if(nome == "juquinha" && senha == "123"){
+    alert("usuario ou senha invalida")
+}
+    
+    alert(nome);
+    alert(senha);
+
+}
